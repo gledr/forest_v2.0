85dd23ad9ca3faadc306a8198a98338cf1cfafdb
Sebastian Pointner <sebastian.pointner@jku.at>
2018-01-25 17:59:36 +0100

    Started to implement Result Injection Pass

--- optim-pass/debugging_pass.cpp
+++ optim-pass/debugging_pass.cpp
@@ -11,8 +11,8 @@
  * |     Compiler:  gcc
  * `-. .--------------------
  *    Y
- *    ♪♪  ,---,
- *   (_,\/_\_/_\     Author:   Pablo González de Aledo (), pablo.aledo@gmail.com
+ *      ,---,
+ *   (_,\/_\_/_\     Author:   Pablo Gonzlez de Aledo (), pablo.aledo@gmail.com
  *     \.\_/_\_/>    Company:  Universidad de Cantabria
  *     '-'   '-'
  * =====================================================================================
@@ -43,12 +43,6 @@
 #include <map>
 #include <set>
 
-
-#define mod_iterator(mod, fn) for( Module::iterator        fn = mod.begin(),  function_end    = mod.end();  fn != function_end;    ++fn )
-#define glo_iterator(mod, gl) for( Module::global_iterator gl = mod.global_begin(), gl_end    = mod.global_end();  gl != gl_end;   ++gl )
-#define fun_iterator(fun, bb) for( Function::iterator      bb = fun->begin(), block_end       = fun->end(); bb != block_end;       ++bb )
-#define blk_iterator(blk, in) for( BasicBlock::iterator    in = blk->begin(), instruction_end = blk->end(); in != instruction_end; ++in )
-#define UNDERSCORE "_"
 
 using namespace llvm;
 using namespace std;
@@ -87,7 +81,7 @@ namespace {
       LoopInfo & LI = LIWP->getLoopInfo();
       
       for (LoopInfo::iterator i = LI.begin(), e = LI.end(); i!=e; ++i){
-	AnalyseLoop(*i, 0);
+		AnalyseLoop(*i, 0);
       }
 #ifdef DEBUG
       errs () << "Found " << BinInstructionsLoopLatch.size() << " binary statementes in loops\n";
@@ -101,7 +95,7 @@ namespace {
       
       for (; I != L->getLoopLatch()->end(); ++ I){
       	if(BinaryOperator::classof(I)){
-	  BinInstructionsLoopLatch.push_back(I);
+		  BinInstructionsLoopLatch.push_back(I);
       	}
       }
     }
@@ -122,54 +116,54 @@ struct SelectVariables: public ModulePass {
  
     vector<ReplaceAfter> values_to_replace;
     
-    mod_iterator(M, fn) {
-      fun_iterator(fn, bb){
-	blk_iterator(bb, in){
+    for (Module::iterator fn = M.begin(); fn != M.end(); ++fn ){
+	  for (Function::iterator bb = fn->begin(); bb != fn->end(); ++bb){
+		for (BasicBlock::iterator in = bb->begin(); in != bb->end(); ++in){
 
-	  if(BinaryOperator::classof(in)){
+		  if(BinaryOperator::classof(in)){
 #ifdef DEBUG
-	    errs () << "Binary Operator Detected!\n";
-	    errs () << *in << "\n";
+			errs () << "Binary Operator Detected!\n";
+			errs () << *in << "\n";
 #endif
-	    // Search in the extracted data by loop_latch_info if the binary instruction is part of a loop latch.
-	    // Iff it is part of a loop latch we are not inserting select variables, since this would end up in an endless loop
-	    // as the SAT solver can insert arbitrary values and the loop will never terminate
-	    if(std::find(BinInstructionsLoopLatch.begin(), BinInstructionsLoopLatch.end(), in) == BinInstructionsLoopLatch.end()){
+			// Search in the extracted data by loop_latch_info if the binary instruction is part of a loop latch.
+			// Iff it is part of a loop latch we are not inserting select variables, since this would end up in an endless loop
+			// as the SAT solver can insert arbitrary values and the loop will never terminate
+			if(std::find(BinInstructionsLoopLatch.begin(), BinInstructionsLoopLatch.end(), in) == BinInstructionsLoopLatch.end()){
 #ifdef DEBUG
-	      errs () << "No loop latch is using this\n";
+			  errs () << "No loop latch is using this\n";
 #endif
-	      BasicBlock::iterator insertpos = in; insertpos++;
+			  BasicBlock::iterator insertpos = in; insertpos++;
 	      
-	      AllocaInst* enable_ptr = new AllocaInst(Type::getInt1Ty( M.getContext() ), "select_enable", insertpos);
-	      AllocaInst* val_ptr    = new AllocaInst(in->getType(), "select_value", insertpos);
+			  AllocaInst* enable_ptr = new AllocaInst(Type::getInt1Ty( M.getContext() ), "select_enable", insertpos);
+			  AllocaInst* val_ptr    = new AllocaInst(in->getType(), "select_value", insertpos);
 	
-	      LoadInst* enable = new LoadInst(enable_ptr,"",insertpos);
-	      LoadInst* val = new LoadInst(val_ptr,"",insertpos);
+			  LoadInst* enable = new LoadInst(enable_ptr,"",insertpos);
+			  LoadInst* val = new LoadInst(val_ptr,"",insertpos);
 	      
-	      SelectInst * SelectInstruction =  SelectInst::Create (enable,val, in, "select_result", insertpos);
+			  SelectInst * SelectInstruction =  SelectInst::Create (enable,val, in, "select_result", insertpos);
 
-	      // As we manipulate the result of a binary instruction we also have to make sure, that our introduces result
-	      // ends up in the indtended register!
-	      ReplaceAfter val_to_repl = {in, SelectInstruction };
-	      values_to_replace.push_back(val_to_repl);
+			  // As we manipulate the result of a binary instruction we also have to make sure, that our introduces result
+			  // ends up in the indtended register!
+			  ReplaceAfter val_to_repl = {in, SelectInstruction };
+			  values_to_replace.push_back(val_to_repl);
 	      
-	    } else {
+			} else {
 #ifdef DEBUG
-	      errs () << "Instruction used in loop latch!\n";
+			  errs () << "Instruction used in loop latch!\n";
 #endif
-	    }
+			}
 #ifdef DEBUG
-	    errs () << "Value ID: " << in->getValueID() << "\n";
-	    errs () << "Address" << in << "\n";
+			errs () << "Value ID: " << in->getValueID() << "\n";
+			errs () << "Address" << in << "\n";
 #endif	   
-	  }
-	  else if (ICmpInst::classof(in)){
+		  }
+		  else if (ICmpInst::classof(in)){
 #ifdef DEBUG
-	    errs () << "Compare Instruction Detected!\n";
-	    errs () << *in << "\n";
+			errs () << "Compare Instruction Detected!\n";
+			errs () << *in << "\n";
 #endif
-	  }
-	}
+		  }
+		}
       }
     }
     
@@ -179,21 +173,94 @@ struct SelectVariables: public ModulePass {
       Instruction* replace_by = it->replace_by;
 
       for (Value::user_iterator i = instr_to_replace->user_begin(), e = instr_to_replace->user_end(); i != e; ++i){
-	Instruction *instruction = dyn_cast<Instruction>( *i );
+		Instruction *instruction = dyn_cast<Instruction>( *i );
 
-	if( instruction == replace_by ) continue;
+		if( instruction == replace_by ) continue;
 
-	for(size_t n = 0; n < instruction->getNumOperands(); n++ ){
-	  if( instruction->getOperand(n) == instr_to_replace ){
-	    instruction->setOperand(n, replace_by);
-	  }
-	}
+		for(size_t n = 0; n < instruction->getNumOperands(); n++ ){
+		  if( instruction->getOperand(n) == instr_to_replace ){
+			instruction->setOperand(n, replace_by);
+		  }
+		}
       }
     }
 
     return false;
   }
 };
+
+struct InjectResults : public ModulePass {
+  static char ID;
+  InjectResults() : ModulePass(ID) {}
+
+  virtual bool runOnModule(Module &M) {
+
+	Constant* nondet_int = M.getOrInsertFunction("__VERIFIER_nondet_int" , Type::getInt32Ty(M.getContext()), (Type*)0);
+	Function* hook_value = dyn_cast<Function>(nondet_int);
+
+	Constant* nondet_bool = M.getOrInsertFunction("__VERIFIER_nondet_bool" , Type::getInt1Ty(M.getContext()), (Type*)0);
+	Function* hook_enable = dyn_cast<Function>(nondet_bool);
+	
+	int select_var_pos = 0;	
+	for (Module::iterator fn = M.begin(); fn != M.end(); ++fn ){
+	  for (Function::iterator bb = fn->begin(); bb != fn->end(); ++bb){
+		for (BasicBlock::iterator in = bb->begin(); in != bb->end(); ++in){
+		  	select_var_pos++;
+		  if(SelectInst::classof(in)){
+			break;
+		  }
+		}
+		int cnt = 0;
+		Instruction* alloc_enable;
+		for(BasicBlock::iterator in = bb->begin(); cnt != select_var_pos-4; ++in){
+		  ++cnt;
+		  alloc_enable = in;
+		}
+		errs () << *alloc_enable << "\n";
+
+		cnt = 0;
+		Instruction * alloc_value;
+		for(BasicBlock::iterator in = bb->begin(); cnt != select_var_pos-3; ++in){
+		  ++cnt;
+		  alloc_value = in;
+		}
+		errs () << *alloc_value << "\n";
+
+		cnt = 0;
+		Instruction * top_load_enable;
+		for(BasicBlock::iterator in = bb->begin(); cnt != select_var_pos-2; ++in){
+		  ++cnt;
+		 top_load_enable = in;
+		}
+		
+		errs () << *top_load_enable << "\n";
+
+		cnt = 0;
+		Instruction * top_load_value;
+		for(BasicBlock::iterator in = bb->begin(); cnt != select_var_pos-1; ++in){
+		  ++cnt;
+		 top_load_value = in;
+		}
+		
+		errs () << *top_load_value << "\n";
+
+		Instruction *call_replace_value = CallInst::Create(hook_value, "");
+		call_replace_value->insertAfter(alloc_value);
+
+		Instruction *call_replace_enable = CallInst::Create(hook_enable, "");
+		call_replace_enable->insertAfter(alloc_enable);
+	
+		StoreInst* store_value =  new StoreInst(call_replace_value,  alloc_value,  top_load_value);
+		StoreInst* store_enable = new StoreInst(call_replace_enable, alloc_enable, top_load_enable);
+	  }
+	}
+
+	return true;
+  }
+
+};
+
+
 
 
 // Identifiers
@@ -203,3 +270,6 @@ static RegisterPass<LoopLatchParser> LoopLatchParser(      "loop_latch_info"    
 
 char SelectVariables::ID = 0;
 static RegisterPass<SelectVariables> SelectVariables( "insert_select_variables" , "Inserts the free SAT variables for debugging         " );
+
+char InjectResults::ID = 0;
+static RegisterPass<InjectResults> InjectResults("inject_results", "Inserts results generated by SAT by calling a library function accessing the db");
